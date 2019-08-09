0451875092d905338f105d76913b80cd31b09944
Tom Schofield <tschofield@apptium.com>
2019-08-09 15:48:47 -0400

    moved .flake8

--- app/core/models.py
+++ app/core/models.py
@@ -6,8 +6,19 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         # creates and saves a new user
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('you must provide a valid email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        # create and saves a new superuser
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
