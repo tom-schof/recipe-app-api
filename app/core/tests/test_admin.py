0f3b2e60f15a7218de6a9e3dd63797dc38091189
Tom Schofield <tschofield@apptium.com>
2019-08-12 14:52:35 -0400

    updated user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.admin_user = get_user_model().object.create_superuser(
+        self.admin_user = get_user_model().objects.create_superuser(
             email="admin@londonappdev.com",
             password="passwrod123"
         )
@@ -18,10 +18,24 @@ class AdminSiteTests(TestCase):
 
         )
 
-        def test_users_listed(self):
-            # Test that users are listed on the user page
-            url = reverse("admin:core_user_changelist")
-            res = self.client.get(url)
+    def test_users_listed(self):
+        # Test that users are listed on the user page
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
 
-            self.assertContains(res, self.user.name)
-            self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        # test that the user edi tpage works
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        # Test that the create user page works
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
